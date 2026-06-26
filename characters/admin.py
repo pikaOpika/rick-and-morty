e1d77154de2fbfe5615bfc42c58328ea5123ecfd
Vlad <vlad@exampl.com>
2026-06-26 10:55:53 +0300

    created model character and added db.sqlite3 to .gitignore

--- characters/admin.py
+++ characters/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from characters.models import Character
 
-# Register your models here.
+
+admin.site.register(Character)
--- characters/models.py
+++ characters/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Character(models.Model):
+    class StatusChoices(models.TextChoices):
+        ALIVE = 'Alive'
+        DEAD = 'dead',
+        UNKNOWN = 'unknown' 
+
+    class GenderChoices(models.TextChoices):
+        MALE = 'M', 'Male'
+        FEMALE = 'F', 'Female'
+        GENDERLESS = 'G', 'Genderless'
+        UNKNOWN = 'U', 'Unknown' 
+
+    api_id = models.IntegerField(unique=True)
+    name = models.CharField(max_length=100)
+    status = models.CharField(max_length=50, choices=StatusChoices)
+    species = models.CharField(max_length=100)
+    gender = models.CharField(max_length=55, choices=GenderChoices)
+    image = models.URLField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
